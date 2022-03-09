315e864e737e9cd90dfdf4c7b078f6ae0ffc30ca
Your Name <kalaitzidispaul@gmail.com>
2022-03-09 18:36:24 -0500

    created html and back ticks

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
+const fs = require('fs')
+const generatePage = require('./src/page-template.js')
 const profileDataArgs = process.argv.slice(2, process.argv.length)
-console.log(profileDataArgs)
 
-const printProfileData = (profileDataArr) => {
-    //this..
-   for(let i = 0; i <profileDataArr.length; i++){
-       console.log(profileDataArr[i])
-   }
-   console.log('===============');
+const [nameOne, github] = profileDataArgs
 
-   //Is the same as this
-   profileDataArr.forEach((profileItem) => console.log(profileItem))
-}
 
-printProfileData(profileDataArgs)
+fs.writeFile('index.html', generatePage(nameOne, github), err =>{
+    if(err) throw new err
+
+    console.log('Portfolio Complete! Check out index.html to see the output')
+})
